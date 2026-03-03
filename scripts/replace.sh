82ac81ef30a8ff1eeb4028a5f647a7911ce04e6e
Ishiyama Yuzuki <yuzupon1480@gmail.com>
2026-03-04 02:19:08 +0900

    fix: restructure command construction in replace.sh for clarity

--- scripts/replace.sh
+++ scripts/replace.sh
@@ -8,4 +8,9 @@ SID_NS2_DX4=fd00:3::2
 SID_NS3_END=fd00:3::3
 SID_NS4_DX4=fd00:3::4
 
-ip -n ns3 -6 route $OPERATION $SID_NS3_END/128 encap bpf xmit obj bin/parse.bpf.o section lwt_xmit dev veth1@ns3
+CMD="ip -n ns3 -6 route $OPERATION $SID_NS3_END/128"
+if [[ "$OPERATION" == "add" || "$OPERATION" == "rep" ]]; then
+	CMD+=" encap bpf xmit obj bin/parse.bpf.o section lwt_xmit dev veth1@ns3"
+fi
+
+eval "$CMD"
